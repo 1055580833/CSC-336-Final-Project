8e496126ee0cbd3d66a4d07e0c7aa1dc12b84cd3
Haozhe Wu <70547760+1055580833@users.noreply.github.com>
2021-04-27 07:08:23 -0400

    Add files via upload

--- Apple_Price_Compare.js
+++ Apple_Price_Compare.js
@@ -2,14 +2,35 @@
     "use strict";
 
     window.onload = function(){
+        gallery();
 
     }
 
+    //Change the main image automatically
+    function gallery(){
+        let gallery = document.getElementById("imageGallery");
+    //    Get the branch
 
-    function gallery(){    //Change the main image automatically
+
+        let start = true;
+        let add = setInterval(function (){
+            if(start) {
+                let old = document.getElementById("mainPicture");
+                old.parentNode.removeChild(old);
+
+                //Randomly change the image
+                let random = parseInt(Math.random() * 10);
+                let img = document.createElement("img");
+                img.id = "mainPicture"
+                img.src = "img/" + random + ".jpeg";
+                gallery.append(img);
+            }else{
+                clearInterval(add)
+            }
+        },4000)
 
 
     }
 
-})
+})();
 
