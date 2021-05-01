d42bc18a4574efe89b8c1f46ca6653ce8163099f
Haozhe Wu <70547760+1055580833@users.noreply.github.com>
2021-05-01 17:14:50 -0400

    Add files via upload

--- MacBookPro13.js
+++ MacBookPro13.js
@@ -9,7 +9,7 @@
         fetch("https://amazon-price1.p.rapidapi.com/search?marketplace=US&keywords=2020%20Apple%20MacBook%20Pro%20with%20Apple%20M1%20Chip%20(13-inch%2C%208GB%20RAM%2C%20256GB%20SSD%20Storage)%20-%20Space%20Gray", {
         "method": "GET",
         "headers": {
-        	"x-rapidapi-key": "9eabc92e42mshcbe2969f600a38bp1162f5jsn52a0ef3d2cdf",
+        	"x-rapidapi-key": "ede3e37a32msh7ee72e454d92ddcp170afcjsn8d76ea6ebec4",
         	"x-rapidapi-host": "amazon-price1.p.rapidapi.com"
         }
         })
@@ -28,7 +28,7 @@
         fetch("https://bestbuy-products.p.rapidapi.com/product?url=https%3A%2F%2Fwww.bestbuy.com%2Fsite%2Fmacbook-pro-13-3-laptop-apple-m1-chip-8gb-memory-256gb-ssd-latest-model-space-gray%2F6418601.p%3FskuId%3D6418601&sku=5748618", {
         "method": "GET",
         "headers": {
-        	"x-rapidapi-key": "ede3e37a32msh7ee72e454d92ddcp170afcjsn8d76ea6ebec4",
+        	"x-rapidapi-key": "9eabc92e42mshcbe2969f600a38bp1162f5jsn52a0ef3d2cdf",
         	"x-rapidapi-host": "bestbuy-products.p.rapidapi.com"
         }
         })
--- MacbookAir.js
+++ MacbookAir.js
@@ -6,18 +6,18 @@
         
     //fetch price from amazon.
 
-    // fetch("https://amazon-price1.p.rapidapi.com/search?marketplace=US&keywords=M1%20MacBook%20Air", {
-	// "method": "GET",
-	// "headers": {
-	// 	"x-rapidapi-key": "ede3e37a32msh7ee72e454d92ddcp170afcjsn8d76ea6ebec4",
-	// 	"x-rapidapi-host": "amazon-price1.p.rapidapi.com"
-	// }
-    // })
-    //     .then(response => response.json())
-    //     .then(displayData)
-    //     .catch(err => {
-	//        console.error(err);
-    // });
+    fetch("https://amazon-price1.p.rapidapi.com/search?marketplace=US&keywords=M1%20MacBook%20Air", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "ede3e37a32msh7ee72e454d92ddcp170afcjsn8d76ea6ebec4",
+		"x-rapidapi-host": "amazon-price1.p.rapidapi.com"
+	}
+    })
+        .then(response => response.json())
+        .then(displayData)
+        .catch(err => {
+	       console.error(err);
+    });
 
         function displayData(response){
             document.getElementById('item-desc1').textContent = response[0].title;
@@ -25,18 +25,18 @@
         }
       
     // fetch price from Best Buy
-    // fetch("https://bestbuy-products.p.rapidapi.com/product?url=https%253A%252F%252Fwww.bestbuy.com%252Fsite%252Fmacbook-air-13-3-laptop-apple-m1-chip-8gb-memory-256gb-ssd-latest-model-space-gray%252F5721600.p%253FskuId%253D5721600&sku=5721600", {
-	// "method": "GET",
-	// "headers": {
-	// 	"x-rapidapi-key": "ede3e37a32msh7ee72e454d92ddcp170afcjsn8d76ea6ebec4",
-	// 	"x-rapidapi-host": "bestbuy-products.p.rapidapi.com"
-	// }
-    // })
-    // .then(response => response.json())
-    // .then(displayData1)
-    // .catch(err => {
-	//        console.error(err);
-    // });
+    fetch("https://bestbuy-products.p.rapidapi.com/product?url=https%253A%252F%252Fwww.bestbuy.com%252Fsite%252Fmacbook-air-13-3-laptop-apple-m1-chip-8gb-memory-256gb-ssd-latest-model-space-gray%252F5721600.p%253FskuId%253D5721600&sku=5721600", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "ede3e37a32msh7ee72e454d92ddcp170afcjsn8d76ea6ebec4",
+		"x-rapidapi-host": "bestbuy-products.p.rapidapi.com"
+	}
+    })
+    .then(response => response.json())
+    .then(displayData1)
+    .catch(err => {
+	       console.error(err);
+    });
 
         function displayData1(response){
         document.getElementById('bestbuy').textContent = response.prices.currency+response.prices.current_price;
